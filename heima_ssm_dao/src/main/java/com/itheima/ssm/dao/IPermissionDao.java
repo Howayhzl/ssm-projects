8657428354535931a17a84a5232be863a5edf17b
Howay <heweihzl@163.com>
2019-08-10 13:26:12 +0800

    day04 查询所有资源权限

--- heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IPermissionDao.java
+++ heima_ssm_dao/src/main/java/com/itheima/ssm/dao/IPermissionDao.java
@@ -10,4 +10,7 @@ public interface IPermissionDao {
 
     @Select("select * from permission where id in (select permissionId from role_permission where roleId=#{id} )")
     public List<Permission> findPermissionByRoleId(String id) throws Exception;
+
+    @Select("select * from permission")
+    List<Permission> findAll() throws Exception;
 }
